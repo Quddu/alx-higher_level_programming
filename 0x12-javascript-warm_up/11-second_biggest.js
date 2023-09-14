e5f30c870877e4031f50953f5a6bc70502202153
Quddu <Quddu@github.com>
2023-09-13 19:35:34 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+const myList = process.argv.slice(2);
+if (!myList.sort((a, b) => b - a)[1]) {
   console.log(0);
 } else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
-}}
+  console.log(myList.sort((a, b) => b - a)[1]);
+}
