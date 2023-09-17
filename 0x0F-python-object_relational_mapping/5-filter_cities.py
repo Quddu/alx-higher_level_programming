5dcd9a9af899b54ec9081e97c2bd927a13397299
Quddu <Quddu@github.com>
2023-09-16 18:17:14 -0700

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Write a script that lists all cities from the database
- hbtn_0e_4_usa"""
+""" script that takes in the name of a state as an argument and
+ lists all cities of that state, using the database
+  hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
@@ -14,14 +15,18 @@ if __name__ == "__main__":
      need to create a cursor object"""
     cur = db.cursor()
     """The execute function requires one parameter, the query."""
-    cur.execute("SELECT cities.id, cities.name, states.name\
+    cur.execute("SELECT cities.name\
         FROM cities\
         JOIN states\
-        ON cities.state_id=states.id")
+        ON cities.state_id=states.id\
+        WHERE BINARY states.name = %s\
+        ORDER BY cities.id ASC;", (sys.argv[4], ))
     """Obtaining Query Results"""
     rows = cur.fetchall()
+    cities_list = []
     for row in rows:
-        print(row)
+        cities_list.append(row[0])
+    print(", ".join(cities_list))
     """ Close all cursors"""
     cur.close()
     """Close all databases"""
