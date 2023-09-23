b829d3047d116c0448835e22dcb41f4219dc6f37
Quddu <Quddu@github.com>
2023-09-22 20:06:34 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-""" cript that takes in an argument and displays all values in
- the states table of hbtn_0e_0_usa where name matches the
-  argument."""
-import MySQLdb
+
+"""Module that lists all states from the hbtn_0e_0_usa database."""
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost',
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         port=3306)
-    """In order to put our new connnection to good use we
-     need to create a cursor object"""
-    cur = db.cursor()
-    """The execute function requires one parameter, the query."""
-    cur.execute("SELECT * FROM states\
-        WHERE BINARY name = '{}'\
-            ORDER BY id ASC".format(sys.argv[4]))
-    """Obtaining Query Results"""
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    """ Close all cursors"""
-    cur.close()
-    """Close all databases"""
-    db.close()
+
+    # Get MySQL credentials and search name from command-line arguments
+    # and # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+
+    # Execute the SQL query to retrieve states with the specified name
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+
+    # Fetch all rows and print the states
+    [print(state) for state in c.fetchall()]
+
