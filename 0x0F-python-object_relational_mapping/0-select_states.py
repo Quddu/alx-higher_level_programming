568711e5e153f196551a5e463a8ffa6fe21dc081
Quddu <Quddu@github.com>
2023-12-15 01:15:46 -0800

    Done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-""" This script will list all state in the database """
+"""holds one function"""
 
-from sys import argv
 import MySQLdb
+from sys import argv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """lists all states from the database hbtn_0e_usa"""
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        db=argv[3])
 
-    db_user = argv[1]
-    db_password = argv[2]
-    db_name = argv[3]
+    curs = database.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
 
-    database = MySQLdb.connect(host='localhost',
-                               port=3306,
-                               user=db_user,
-                               password=db_password,
-                               db=db_name)
-
-    cursor = database.cursor()
-
-    cursor.execute('SELECT id, name FROM states ORDER BY states.id ASC')
-
-    for row in cursor.fetchall():
+    a = curs.fetchall()
+    for row in a:
         print(row)
+    curs.close()
+    database.close()
