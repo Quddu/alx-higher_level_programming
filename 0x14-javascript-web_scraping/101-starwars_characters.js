08d902363f68284fcb034fdf42bfe09b173c19ec
Quddu <Quddu@github.com>
2024-02-01 02:46:15 +0000

    Done

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,44 @@
+t request = require('request');
+
+function getDataFrom (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, _res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+function errHandler (err) {
+  console.log(err);
+}
+
+function printMovieCharacters (movieId) {
+  const movieUri = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  getDataFrom(movieUri)
+    .then(JSON.parse, errHandler)
+    .then(function (res) {
+      const characters = res.characters;
+      const promises = [];
+
+      for (let j = 0; j < characters.length; ++j) {
+        promises.push(getDataFrom(characters[j]));
+      }
+
+      Promise.all(promises)
+        .then((results) => {
+          for (let j = 0; j < results.length; ++j) {
+            console.log(JSON.parse(results[j]).name);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+}
+
+printMovieCharacters(process.argv[2]);
