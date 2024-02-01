0cd83087d5597fda92c038fcf34dd9a56f43179e
Quddu <Quddu@github.com>
2024-02-01 02:23:10 +0000

    Done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
-const fs = require('fs');
-const filePath = process.argv[2];
+import sys
 
-fs.readFile(filePath, 'utf-8', (error, data) => {
-  if (error) { console.log(error); } else { console.log(data); }
-});
+def read_file_content(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            print(content)
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script_name.py <file_path>")
+    else:
+        file_path = sys.argv[1]
+        read_file_content(file_path)
