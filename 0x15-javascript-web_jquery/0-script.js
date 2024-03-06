81948f20c4126e6a4b992b21d30c1a31eb385b58
Quddu <Quddu@github.com>
2024-03-06 02:43:35 +0000

    Done

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-const headers = document.getElementsByTagName('header');
-if (headers.length > 0) {
-    const header = headers[0];
-    header.style.color = '#FF0000';
-}
+$('header').css('color', '#FF0000');
